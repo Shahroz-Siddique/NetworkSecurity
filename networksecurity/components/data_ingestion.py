0a249df7934044b00c4a280f984867b01ba8a878
shahroz_siddique <shhrzsiddique@gmail.com>
2025-04-21 17:43:44 +0500

    Data Validation

--- networksecurity/components/data_ingestion.py
+++ networksecurity/components/data_ingestion.py
@@ -1,42 +1,42 @@
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
 
-# configuration of the Data INgestion Config
 
+## configuration of the Data Ingestion Config
 
 from networksecurity.entity.config_entity import DataIngestionConfig
 from networksecurity.entity.artifact_entity import DataIngestionArtifact
-import os 
+import os
 import sys
 import numpy as np
 import pandas as pd
 import pymongo
 from typing import List
 from sklearn.model_selection import train_test_split
-
 from dotenv import load_dotenv
 load_dotenv()
 
-MONGO_DB_URL = os.getenv("MONGO_DB_URL")
+MONGO_DB_URL=os.getenv("MONGO_DB_URL")
 
 
 class DataIngestion:
     def __init__(self,data_ingestion_config:DataIngestionConfig):
         try:
-            self.data_ingestion_config =data_ingestion_config
+            self.data_ingestion_config=data_ingestion_config
         except Exception as e:
             raise NetworkSecurityException(e,sys)
-    
+        
     def export_collection_as_dataframe(self):
-        # read data from mongodb
+        """
+        Read data from mongodb
+        """
         try:
-            database_name= self.data_ingestion_config.database_name
-            collection_name = self.data_ingestion_config.collection_name
-            self.momgo_client= pymongo.MongoClient(MONGO_DB_URL)
-            collection = self.momgo_client[database_name][collection_name]
+            database_name=self.data_ingestion_config.database_name
+            collection_name=self.data_ingestion_config.collection_name
+            self.mongo_client=pymongo.MongoClient(MONGO_DB_URL)
+            collection=self.mongo_client[database_name][collection_name]
 
-
-            df= pd.DataFrame(list(collection.find()))
+            df=pd.DataFrame(list(collection.find()))
             if "_id" in df.columns.to_list():
                 df=df.drop(columns=["_id"],axis=1)
             
@@ -44,7 +44,7 @@ class DataIngestion:
             return df
         except Exception as e:
             raise NetworkSecurityException
-
+        
     def export_data_into_feature_store(self,dataframe: pd.DataFrame):
         try:
             feature_store_file_path=self.data_ingestion_config.feature_store_file_path
@@ -86,14 +86,16 @@ class DataIngestion:
             
         except Exception as e:
             raise NetworkSecurityException(e,sys)
-
-    def intiate_data_ingestion(self):
+        
+        
+    def initiate_data_ingestion(self):
         try:
-            dataframe= self.export_collection_as_dataframe()
+            dataframe=self.export_collection_as_dataframe()
             dataframe=self.export_data_into_feature_store(dataframe)
             self.split_data_as_train_test(dataframe)
-            dataingestionartifact=DataIngestionArtifact(trained_file_path= self.data_ingestion_config.training_file_path,
+            dataingestionartifact=DataIngestionArtifact(trained_file_path=self.data_ingestion_config.training_file_path,
                                                         test_file_path=self.data_ingestion_config.testing_file_path)
+            return dataingestionartifact
 
         except Exception as e:
             raise NetworkSecurityException
